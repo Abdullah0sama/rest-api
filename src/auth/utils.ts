46bb437ac376ac72a29c9f48bc71c181b3314610
Abdullah <abdullahosama15@gmail.com>
2023-03-01 21:53:06 +0200

    cleaning up

--- src/auth/utils.ts
+++ src/auth/utils.ts
@@ -1,6 +1,11 @@
 import * as bcrypt from 'bcrypt';
 
 const SALT_ROUNDS = 12;
+/**
+ * Hasher
+ * @param payload
+ * @returns hashed payload
+ */
 export const hashPayload = (payload: string) => {
   return bcrypt.hash(payload, SALT_ROUNDS);
 };
